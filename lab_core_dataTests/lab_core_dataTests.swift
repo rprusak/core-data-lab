dd4b539966b31f0c56901f42a7923bf102794d5f
r <r@rs-mac.local>
2018-01-03 02:21:07 -0800

    Initial Commit

---
+++ lab_core_dataTests/lab_core_dataTests.swift
@@ -0,0 +1,36 @@
+//
+//  lab_core_dataTests.swift
+//  lab_core_dataTests
+//
+//  Created by r on 03/01/2018.
+//  Copyright © 2018 r. All rights reserved.
+//
+
+import XCTest
+@testable import lab_core_data
+
+class lab_core_dataTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
